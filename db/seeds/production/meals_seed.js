e095772a8939eb87fcee59b4f3de0c835106b8bd
Boots2291 <buteramv@gmail.com>
2017-12-14 15:20:38 -0700

    Change seeds file path

---
+++ db/seeds/production/meals_seed.js
@@ -0,0 +1,14 @@
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('meals').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('meals').insert([
+        {name: 'Breakfast'},
+        {name: 'Snack'},
+        {name: 'Lunch'},
+        {name: 'Dinner'}
+      ]);
+    });
+};
