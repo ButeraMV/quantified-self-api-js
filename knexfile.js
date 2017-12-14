2d6c7299d768e3dbc827f22d933d1ab098d6855e
Boots2291 <buteramv@gmail.com>
2017-12-14 15:34:19 -0700

    Heroku troubleshooting

--- knexfile.js
+++ knexfile.js
@@ -30,7 +30,7 @@ module.exports = {
       directory: './db/migrations'
     },
     seeds: {
-      directory: './db/seeds/dev'
+      directory: './db/seeds/production'
     },
     useNullAsDefault: true
   }
